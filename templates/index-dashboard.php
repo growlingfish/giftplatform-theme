d5602eb77816a12acf319dbfa1f678ff4c803c9d
Ben Bedwell <github@growlingfish.com>
2017-07-21 09:03:39 +0100

    Sprint

--- templates/index-dashboard.php
+++ templates/index-dashboard.php
@@ -8,8 +8,15 @@
  */
 
 ?>
+
+<div class="center-logo">
+    <a href="<php echo wp_login_url(); ?>">
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/white-icon-circle-text.png" />
+    </a>
+</div>
+
 <script>
 jQuery(function($) {
-		
+	$.backstretch('<?php echo get_stylesheet_directory_uri(); ?>/images/backstretch/index-welcome.jpg');	
 });
 </script>
