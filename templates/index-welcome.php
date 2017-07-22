b61bdeeb4a615e4dc5dc57710728bed84e1317d6
Ben Bedwell <github@growlingfish.com>
2017-07-22 14:53:07 +0100

    Sprint

--- templates/index-welcome.php
+++ templates/index-welcome.php
@@ -8,8 +8,19 @@
  */
 
 ?>
+
+<div class="head-logo">
+    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/white-icon-circle-text.png" />
+</div>
+
+<div class="step" id="step1">
+    <h1></h1>
+</div>
+
 <script>
 jQuery(function($) {
-	$.backstretch('<?php echo get_stylesheet_directory_uri(); ?>/images/backstretch/index-welcome.jpg');	
+	$.backstretch('<?php echo get_stylesheet_directory_uri(); ?>/images/backstretch/index-welcome.jpg');
+
+    $('#step1').fadeIn();
 });
 </script>
