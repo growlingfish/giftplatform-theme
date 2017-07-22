1ee8eecc59a06046d31a12521b1ab5f7c9b57f87
Ben Bedwell <github@growlingfish.com>
2017-07-22 15:55:38 +0100

    Sprint

--- templates/index-welcome.php
+++ templates/index-welcome.php
@@ -17,14 +17,14 @@
     <h1>Welcome!</h1>
     <p>The GIFT Platform is a set of tools, including this website, that lets you send and receive 'gifts'.</p>
     <p>It is a service provided by the EU-funded GIFT project, which is investigating ways to combine digital content with real cultural artifacts - museum exhibits, historical sites, and so on - to provide new ways for visitors to engage with heritage.</p>
-    <button onclick="step2();">Continue</button>
+    <button onclick="goToStep2();">Continue</button>
 </div>
 
 <div class="step" id="step2">
     <h1>What is this website?</h1>
     <p>This website lets you make a gift for another user. The gifts are personal messages that you create for another user to experience when they have found particular exhibits in the museum.</p>
     <p>Using this website you choose the exhibit, create the gift, and send it to the lucky recipient.</p>
-    <button onclick="step3();">OK</button>
+    <button onclick="goToStep3();">OK</button>
 </div>
 
 <div class="step" id="step3">
@@ -38,13 +38,13 @@ jQuery(function($) {
 
     $('#step1').fadeIn();
 
-    function step2 () {
+    function goToStep2 () {
         jQuery('#step1').slideDown(function () {
             jQuery('#step2').slideUp();
         });
     }
 
-    function step3 () {
+    function goToStep3 () {
         jQuery('#step2').slideDown(function () {
             jQuery('#step3').slideUp();
         });
