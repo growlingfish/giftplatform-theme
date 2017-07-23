a9f76043325bcc922756ca50b746865a3f350c6f
Ben Bedwell <github@growlingfish.com>
2017-07-23 11:44:56 +0100

    Sprint

--- templates/index-dashboard.php
+++ templates/index-dashboard.php
@@ -59,7 +59,7 @@ $user = wp_get_current_user();
 
 <div class="step" id="step3">
     <h1>Choose an Exhibit</h1>
-    <p>Which museum exhibit would you like to include in your gift for <span id="receiverName">the receiver</span>?</p>
+    <p>Which museum exhibit would you like to include in your gift for <span class="receiverName">the receiver</span>?</p>
     <div>
 <?php 
 	$query = array(
@@ -75,7 +75,7 @@ $user = wp_get_current_user();
                 .'<h2>'.$object->post_title.'</h2>'
 			    .'<img src="'.get_the_post_thumbnail_url($object->ID, 'medium').'" />'
 			    .'<div>'.wpautop($object->post_content).'</div>'
-                .'<button id="step4_button" object="'.$object->ID.'" objectImage="'.get_the_post_thumbnail_url($object->ID, 'large').'">Choose</button>'
+                .'<button class="step4_button" object="'.$object->ID.'" objectImage="'.get_the_post_thumbnail_url($object->ID, 'large').'">Choose</button>'
             .'</div>';
 		}
 	}
@@ -84,7 +84,7 @@ $user = wp_get_current_user();
 </div>
 
 <div class="step" id="step4">
-    <img id="exhibitImage" src="" />
+    <img class="exhibitImage" src="" />
     <h2>A New Name</h2>
     <p>If <em>you</em> could name this object what would you call it?</p>
     <p><label for="exhibitName">Name:</label><input type="text" name="exhibitName" id="exhibitName"></p>
@@ -93,9 +93,32 @@ $user = wp_get_current_user();
 
 <div class="step" id="step5">
     <h2>Write Them a Card</h2>
-    <p>The gift card will be the first thing the receiver sees, before they start unwrapping the gift.</p>
+    <p>The gift card will be the first thing <span class="receiverName">the receiver</span> sees, before they start unwrapping the gift.</p>
     <p><textarea name="giftcard" id="giftcard">Hey stranger - I wanted to give you ...</textarea></p>
     <button id="step6_button">Submit</button>
+</div>
+
+<div class="step" id="step6">
+    <img class="exhibitImage" src="" />
+    <h2>What is in your gift?</h2>
+    <p>Write a message for <span class="receiverName">the receiver</span> about this part of their gift when they have found it in the museum.</p>
+    <p><textarea name="payload" id="payload"></textarea></p>
+    <button id="step7_button">Submit</button>
+</div>
+
+<div class="step" id="step7">
+    <h2>Another?</h2>
+    <p>This part of the gift has been sent to <span class="receiverName">the receiver</span>. Would you like to add another part to the gift?</p>
+    <p>
+        <button id="step8a_button">No, thanks</button>
+        <button id="step8b_button">Yes!</button>
+    </p>
+</div>
+
+<div class="step" id="step8a">
+    <h2>Outro</h2>
+    <p>Outro text</p>
+    <button id="step9_button">Log out</button>
 </div>
 
 <script>
@@ -104,6 +127,7 @@ var apiBase = "https://gifting.digital/wp-json/gift/v1/";
 var receiver;
 var exhibit;
 var exhibitName;
+var giftcard;
 
 jQuery(function($) {
 	$.backstretch('<?php echo get_stylesheet_directory_uri(); ?>/images/backstretch/index-welcome.jpg');
@@ -130,12 +154,11 @@ jQuery(function($) {
                 cache: false,
                 url: apiBase + "validate/receiver/" + jQuery('#recipientEmail').val(),
                 method: "GET"
-                //data: { name: "John", location: "Boston" }
             });
             request.done(function( data ) {
                 if (data.success && typeof(data.exists) != 'undefined' && data.exists) {
                     receiver = data.exists;
-                    jQuery('#receiverName').text(receiver.data.display_name);
+                    jQuery('.receiverName').text(receiver.data.display_name);
                     jQuery('#step2a').slideToggle(function () {
                         jQuery('#step3').slideToggle();
                     });
@@ -146,7 +169,6 @@ jQuery(function($) {
                         cache: false,
                         url: apiBase + "new/receiver/" + jQuery('#recipientEmail').val() + "/" + jQuery('#recipientName').val() + "/" + "<?php echo $user->ID; ?>",
                         method: "GET"
-                        //data: { name: "John", location: "Boston" }
                     });
                     setupRequest.done(function( data ) {
                         if (data.success && typeof(data.new) != 'undefined' && data.new) {
@@ -158,7 +180,7 @@ jQuery(function($) {
                                 },
                                 "ID": data.new.id
                             };
-                            jQuery('#receiverName').text(receiver.data.display_name);
+                            jQuery('.receiverName').text(receiver.data.display_name);
                             jQuery('#step2a').slideToggle(function () {
                                 jQuery('#step3').slideToggle();
                             });
@@ -197,12 +219,11 @@ jQuery(function($) {
             cache: false,
             url: apiBase + "validate/receiver/" + 'localbrighton@gifting.digital',
             method: "GET"
-            //data: { name: "John", location: "Boston" }
         });
         request.done(function( data ) {
             if (data.success && typeof(data.exists) != 'undefined' && data.exists) {
                 receiver = data.exists;
-                jQuery('#receiverName').text(receiver.data.display_name);
+                jQuery('.receiverName').text(receiver.data.display_name);
                 jQuery('#step2b').slideToggle(function () {
                     jQuery('#step3').slideToggle();
                 });
@@ -227,12 +248,11 @@ jQuery(function($) {
             cache: false,
             url: apiBase + "validate/receiver/" + 'outoftownbrighton@gifting.digital',
             method: "GET"
-            //data: { name: "John", location: "Boston" }
         });
         request.done(function( data ) {
             if (data.success && typeof(data.exists) != 'undefined' && data.exists) {
                 receiver = data.exists;
-                jQuery('#receiverName').text(receiver.data.display_name);
+                jQuery('.receiverName').text(receiver.data.display_name);
                 jQuery('#step2b').slideToggle(function () {
                     jQuery('#step3').slideToggle();
                 });
@@ -251,13 +271,14 @@ jQuery(function($) {
         });
     });
 
-    $('#step4_button').on('click', function () {
+    $('.step4_button').on('click', function () {
         console.log(jQuery(this).attr('object'));
         exhibit = jQuery(this).attr('object');
         if (!stranger) {
             jQuery('#giftcard').text('Hey ' + receiver.data.display_name + ' - I wanted to give you ...');
         }
-        jQuery('#exhibitImage').attr('src', jQuery(this).attr('objectImage'));
+        jQuery('.exhibitImage').attr('src', jQuery(this).attr('objectImage'));
+        jQuery('.head-logo').hide();
         jQuery('#step3').slideToggle(function () {
             jQuery('#step4').slideToggle();
         });
@@ -266,10 +287,101 @@ jQuery(function($) {
     $('#step5_button').on('click', function () {
         if (jQuery('#exhibitName').val().length > 0 && jQuery('#exhibitName').val().length) {
             exhibitName = jQuery('#exhibitName').val();
+            jQuery('.head-logo').show();
             jQuery('#step4').slideToggle(function () {
                 jQuery('#step5').slideToggle();
             });
         }
     });
+
+    $('#step6_button').on('click', function () {
+        if (jQuery('#giftcard').val().length > 0 && jQuery('#giftcard').val().length) {
+            giftcard = jQuery('#giftcard').val();
+            jQuery('.head-logo').hide();
+            jQuery('#step5').slideToggle(function () {
+                jQuery('#step6').slideToggle();
+            });
+        }
+    });
+
+    $('#step7_button').on('click', function () {
+        if (jQuery('#payload').val().length > 0 && jQuery('#payload').val().length) {
+            payload = jQuery('#payload').val();
+            jQuery('.head-logo').show();
+            jQuery('#step6').slideToggle(function () {
+                jQuery('#step7').slideToggle();
+            });
+        }
+    });
+
+    $('#step8a_button').on('click', function () {
+        sendGift();
+        jQuery('#step7').slideToggle(function () {
+            jQuery('#step8a').slideToggle();
+        });
+    });
+
+    $('#step8b_button').on('click', function () {
+        sendGift();
+        exhibit = null;
+        exhibitName = null;
+        giftcard = null;
+        jQuery('#step7').slideToggle(function () {
+            jQuery('#step3').slideToggle();
+        });
+    });
+
+    $('#step9_button').on('click', function () {
+        window.location.replace("<?php echo wp_logout_url( home_url() ); ?>");
+    });
+    
+    function sendGift () {
+        var request = jQuery.ajax({
+            dataType: "json",
+            cache: false,
+            url: apiBase + "new/gift/",
+            method: "POST"
+            data: { 
+                sender: "<?php echo $user->ID; ?>", 
+                gift: { 
+                    title: exhibitName,
+                    receiver: receiver.data.user_email,
+                    receiverName: receiver.data.display_name,
+                    wraps: [
+                        {}
+                    ],
+                    payloads: [
+                        {
+                            
+                        }
+                    ],
+                    giftcard: {
+                        title: "<?php echo $user->display_name; ?>'s giftcard for " + exhibitName,
+                        content: giftcard
+                    }
+                }
+            }
+        });
+        request.done(function( data ) {
+            if (data.success && typeof(data.exists) != 'undefined' && data.exists) {
+                receiver = data.exists;
+                jQuery('.receiverName').text(receiver.data.display_name);
+                jQuery('#step2b').slideToggle(function () {
+                    jQuery('#step3').slideToggle();
+                });
+            } else {
+                console.log(data);
+                setTimeout(function () {
+                    window.location.replace("https://gifting.digital");
+                }, 3000);
+            }
+        });
+        request.fail(function( jqXHR, textStatus ) {
+            console.log( "Request failed: " + textStatus );
+            setTimeout(function () {
+                window.location.replace("https://gifting.digital");
+            }, 3000);
+        });
+    }
 });
 </script>
