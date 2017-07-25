373aeaa8221f8f8a7600ab90607b2d1b11e8a113
Ben Bedwell <github@growlingfish.com>
2017-07-25 14:46:56 +0100

    Report

--- templates/report-gifts.php
+++ templates/report-gifts.php
@@ -20,9 +20,12 @@
 	foreach ($all_gifts as $gift) {
     ?>
     <div style="border-bottom: 5px solid black; margin-bottom: 20px;">
-        <p>Gift <?php echo $gift->ID; ?></p>
-        <p>Maker: <?php $maker = get_userdata($gift->post_author); echo $maker->user_login.' ('.$gift->post_author')'; ?></p>
+        <h1><?php echo $gift->post_title; ?></h1>
+        <p>Sent at: <?php echo $gift->post_modified; ?></p>
+        <p>ID: <?php echo $gift->ID; ?></p>
+        <p>Maker: <?php $maker = get_userdata($gift->post_author); echo $maker->user_login.' ('.$gift->post_author.')'; ?></p>
         <p>Receiver: <?php $receiver = get_field('receiver', $gift->ID); echo $receiver->user_login.' ('.$receiver->ID.')'; ?></p>
+        
     </div>
     <?php
 	}
