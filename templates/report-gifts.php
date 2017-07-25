16a8ddb6260cef929febe87cdac7fa9c507283be
Ben Bedwell <github@growlingfish.com>
2017-07-25 14:35:01 +0100

    Report

---
+++ templates/report-gifts.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Index page template for users not yet logged in.
+ *
+ * @package giftplatform-theme
+ * @author  Ben Bedwell
+ * @license GPL-3.0+
+ */
+
+?>
+
+<div>
+    <?php 
+    $query = array(
+		'numberposts'   => -1,
+		'post_type'     => 'gift',
+		'post_status'   => 'publish'
+	);
+	$all_gifts = get_posts( $query );
+	foreach ($all_gifts as $gift) {
+         var_dump($gift);
+	}
+    ?>
+</div>
+
+<script>
+jQuery(function($) {
+	
+});
+</script>
