ba054f02fe5ac1987fc61a192a77faca2ddaab6c
Ben Bedwell <github@growlingfish.com>
2017-07-25 19:35:41 +0100

    Report

--- templates/report-gifts.php
+++ templates/report-gifts.php
@@ -9,25 +9,30 @@
 
 ?>
 
-<div>
+<div style="height: 210mm; width: 297mm;">
     <?php 
     $query = array(
 		'numberposts'   => -1,
 		'post_type'     => 'gift',
-		'post_status'   => 'publish'
+		'post_status'   => 'publish',
+        'date_query' => array(
+            array(
+                'after'     => 'July 24th, 2017',
+                'before'    => 'July 28th, 2017',
+                'inclusive' => true,
+            )
+        )
 	);
 	$all_gifts = get_posts( $query );
 	foreach ($all_gifts as $gift) {
     ?>
-    <div style="border-bottom: 5px solid black; margin: 20px 0;">
+    <div style="border-bottom: 5px solid black; margin: 40px 0;">
         <h1><?php echo $gift->post_title; ?></h1>
         <ul>
             <li>Sent at: <?php echo $gift->post_modified; ?></li>
             <li>ID: <?php echo $gift->ID; ?></li>
             <li>Maker: <?php $maker = get_userdata($gift->post_author); echo $maker->nickname.' ('.$gift->post_author.')'; ?></li>
             <li>Receiver: <?php $receiver = get_field('field_58e4f6e88f3d7', $gift->ID); echo $receiver[0]['nickname'].' ('.$receiver[0]['ID'].')'; ?></li>
-            <li>Gift was unwrapped? <?php echo get_field('field_595e0593bd980', $gift->ID); ?></li>
-            <li>Receiver responded? <?php echo get_field('field_595e05c8bd981', $gift->ID); ?></li>
         </ul>
         <h2>Object</h2>
         <?php
@@ -44,20 +49,20 @@
                 }  
                 if ($wrap->unwrap_object) {
                     ?><p><img src="<?php echo get_the_post_thumbnail_url($wrap->unwrap_object->ID, 'large'); ?>" /></p>
-                    <p><?php echo $wrap->unwrap_object->post_title; ?></p><?php
+                    <p>Named: <?php echo $wrap->unwrap_object->post_title; ?></p><?php
                 } else {
                     ?><p>Broken gift ...</p><?php
                 }
             }
         ?>
-        <h2>Gift card</h2>
+        <h2>Gift card (message "before they get their gift")</h2>
         <?php
             $gift->giftcards = get_field('field_5964a5787eb68', $gift->ID);
             foreach ($gift->giftcards as &$giftcard) { ?>
                 <p><?php echo wpautop($giftcard->post_content); ?></p>
             <?php }
         ?>
-        <h2>Gift message (revealed at object)</h2>
+        <h2>Gift (message revealed "at the exhibit")</h2>
         <?php
             $gift->payloads = get_field('field_58e4f689655ef', $gift->ID);
             foreach ($gift->payloads as &$payload) { ?>
