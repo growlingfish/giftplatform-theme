3303552ff813dfb351c296feff38fc806f3f742c
growlingfish <github@growlingfish.com>
2018-05-30 14:08:44 +0100

    Vis

--- templates/report-gift.php
+++ templates/report-gift.php
@@ -49,6 +49,7 @@
 
 <div class="step" id="wraps">
     <h1>Wraps</h1>
+    <p>The recipient was asked to find the following objects:</p>
     <div id="giftobjectsvis" class="grid">
 <?php
     $wraps = get_field( 'field_58e4f5da816ac', $gift->ID);
@@ -65,6 +66,7 @@
             if ($object) {
 
                 echo '<div class="grid-item"><strong>'.$object->post_title.'</strong>';
+                echo '<p>'.get_the_post_thumbnail_url($object->ID, 'medium').'</p>';
 
                 $location = get_field( 'field_59a85fff4be5a', $object->ID );
                 if (!$location || count($location) == 0) {
@@ -88,63 +90,12 @@
             }
         }
     }
+    echo '</div>';
     if (!$wraps) {
         echo '<p style="color: red;">Gift was not wrapped.</p>';
     }
 ?>
-    </div>
 </div>
-
-<?php
-        //venue
-        /*$wraps = get_field( 'field_58e4f5da816ac', $gift->ID);
-        unset($object, $venue);
-        if ($wraps) {
-            foreach ($wraps as $wrap) {
-                $object = get_field( 'field_595b4a2bc9c1c', $wrap->ID);
-                if (is_array($object) && count($object) > 0) {
-                    $object = $object[0];
-                } else if (is_a($object, 'WP_Post')) {
-                        
-                } else {
-                    unset($object);
-                }  
-                if ($object) {
-                    $location = get_field( 'field_59a85fff4be5a', $object->ID );
-                    if (!$location || count($location) == 0) {
-                        
-                    } else {
-                        $location = $location[0];
-                        $venues = wp_get_post_terms( $location->ID, 'venue' );
-                        if ($venues) {
-                            $venue = $venues[0];
-                        }
-                    }
-                }
-            }
-        }
-
-        echo '<div class="grid-item '
-                .(isset ($object->post_title) && isset ($gift->post_modified) && isset ($senderdata->nickname) && isset ($recipientdata->nickname) ? 'complete' : 'incomplete')
-            .'" '
-                .'gift="'.$gift->ID.'"'
-                //.'data-object="'.$object->post_title.'" '
-                .'data-date="'.$gift->post_modified.'" '
-                .'data-sender="'.urldecode($senderdata->nickname).'" '
-                .'data-recipient="'.urldecode($recipientdata->nickname).'" '
-                .'data-venue="'.$venue->name.'" '
-            .'>'
-                .'<strong>Gift #'.$gift->ID.'</strong>'
-                .'<ul>'
-                    .'<li>Sent: '.(isset ($gift->post_modified) ? $gift->post_modified : '<span style="color: red">No date</span>' ).'</li>'
-                    .'<li>By: '.(isset ($senderdata->nickname) ? urldecode($senderdata->nickname) : '<span style="color: red">No sender</span>' ).'</li>'
-                    .'<li>To: '.(isset ($recipientdata->nickname) ? urldecode($recipientdata->nickname) : '<span style="color: red">No recipient</span>' ).'</li>'
-                    //.'<li>Object: '.(isset ($object->post_title) ? $object->post_title : '<span style="color: red">No object</span>' ).'</li>'
-                    .'<li>At: '.(isset ($venue->name) ? $venue->name : 'No venue' ).'</li>'
-                .'</ul>'
-            .'</div>';*/
-
-?>
 
 <script>
 jQuery(function($) {
@@ -153,15 +104,13 @@ jQuery(function($) {
     $('#sent-date').text(moment('<?php echo $gift->post_modified; ?>').format("dddd, MMMM Do YYYY"));
     $('#sent-time').text(moment('<?php echo $gift->post_modified; ?>').format("h:mm:ss a"));
 
-    $('.grid').isotope({
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows'
-    });
-
     $('#detail').fadeIn(function () {
         $('#giftcard').fadeIn(function () {
             $('#wraps').fadeIn(function () {
-
+                $('.grid').isotope({
+                    itemSelector: '.grid-item',
+                    layoutMode: 'fitRows'
+                });
             });
         });
     });
