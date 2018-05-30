b4274dd279946ddf7335083d32d7a712931100e6
growlingfish <github@growlingfish.com>
2018-05-30 15:31:28 +0100

    Vis

--- templates/single-object.php
+++ templates/single-object.php
@@ -1,6 +1,33 @@
 <div class="step" id="detail">
     <h1><?php echo get_the_title(); ?></h1>
 
+    <p><?php echo get_the_post_thumbnail_url('large'); ?></p>
+
+<?php
+    $userdata = get_userdata(get_the_author_meta( 'ID' ));
+?>
+    <p>Created by <?php echo $userdata->nickname; ?> on <?php the_date(); ?>.</p>
+
+<?php
+    $location = get_field( 'field_59a85fff4be5a', get_the_ID() );
+	if (!$location || count($location) == 0) {
+		
+	} else {
+        $location = $location[0];
+        $venues = wp_get_post_terms( $location->ID, 'venue' );
+        foreach ($venues as $v) {
+            $venue = $v->name;
+            break;
+        }
+    }
+?>
+    <p>Located at <?php echo $location->post_title; ?><?php echo (isset($venue) ? ' in ' : ''); ?>.</p>
+
+<?php if (count(get_the_content()) > 0) { ?>
+    <p>Comes with the following description:</p>
+    <blockquote><?php echo wpautop(get_the_content()); ?></blockquote>
+<?php } ?>
+
 </div>
 
 <div style="position: fixed; right: 20px; bottom: 20px;">
