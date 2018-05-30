d9da8aebb1ad0887d3c764ef1f7c249aaef2af6d
growlingfish <github@growlingfish.com>
2018-05-30 11:20:23 +0100

    Vis

--- templates/report-giftsvis.php
+++ templates/report-giftsvis.php
@@ -16,24 +16,71 @@
 
 <?php
     $query = array(
-        'numberposts'   => -1,
-        'post_type'     => 'object',
-        'post_status'   => 'publish'
-    );
-    $all_objects = get_posts( $query );
-    $result = array();
-    foreach ($all_objects as $object) {
-        $location = get_field( 'field_59a85fff4be5a', $object->ID );
-        if (!$location || count($location) == 0) {
-            
-        } else {
-            echo '<div class="grid-item" '
-                    .'data-object="'.$object->ID.'" '
-                .'>'
-                    .$object->post_title
-                .'</div>';
+		'numberposts'   => -1,
+		'post_type'     => 'gift',
+		'post_status'   => 'publish',
+        /*'date_query' => array(
+            array(
+                'after'     => 'July 24th, 2017',
+                'before'    => 'July 28th, 2017',
+                'inclusive' => true,
+            )
+        )*/
+	);
+    $all_gifts = get_posts( $query );
+    foreach ($all_gifts as $gift) {
+        // sender
+        $senderdata = get_userdata($gift->post_author);
+
+        // recipient
+        $recipients = get_field( 'field_58e4f6e88f3d7', $gift->ID );
+        unset($recipientdata);
+        if ($recipients) {
+            foreach ($recipients as $recipient) {
+                $recipientdata = get_userdata($recipient['ID']);
+                break; // only one recipient for now
+            }
         }
+
+        //object + venue
+        $wraps = get_field( 'field_58e4f5da816ac', $gift->ID);
+        unset($object, $venue);
+        if ($wraps) {
+            foreach ($wraps as $wrap) {
+                $object = get_field( 'field_595b4a2bc9c1c', $wrap->ID);
+                if (is_array($object) && count($object) > 0) {
+                    $object = $object[0];
+                } else if (is_a($object, 'WP_Post')) {
+                        
+                } else {
+                    unset($object);
+                }  
+                if ($object) {
+                    $location = get_field( 'field_59a85fff4be5a', $object->ID );
+                    if (!$location || count($location) == 0) {
+                        
+                    } else {
+                        $location = $location[0];
+                        $venues = wp_get_post_terms( $location->ID, 'venue' );
+                        if ($venues) {
+                            $venue = $venue[0];
+                        }
+                    }
+                }
+            }
+        }
+
+        echo '<div class="grid-item" '
+                .'data-object="'.$object->post_title.'" '
+                .'data-date="'.$gift->post_modified.'" '
+                .'data-sender="'.urldecode($senderdata->nickname).'" '
+                .'data-recipient="'.urldecode($recipientdata->nickname).'" '
+                .'data-venue="'.$venue->name.'" '
+            .'>'
+                .$gift->post_title
+            .'</div>';
     }
+
 ?>
 
 </div>
@@ -46,7 +93,11 @@ jQuery(function($) {
         itemSelector: '.grid-item',
         layoutMode: 'fitRows',
         getSortData: {
-            byObject: '[data-object]'
+            byObject: '[data-object]',
+            byDate: '[data-date]',
+            bySender: '[data-sender]',
+            byRecipient: '[data-recipient]',
+            byVenue: '[data-venue]',
         }
     });
 
