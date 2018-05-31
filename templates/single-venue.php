4e1281b06800983a1b809ffe5c7b8ecb9bb97f85
growlingfish <github@growlingfish.com>
2018-05-31 13:28:05 +0100

    Venue

--- templates/single-venue.php
+++ templates/single-venue.php
@@ -4,8 +4,35 @@
 
 <script src="https://unpkg.com/isotope-layout@3.0.6/dist/isotope.pkgd.min.js"></script>
 
-<div class="step" id="venue">
-    <h1><?php var_dump($venue); ?></h1>
+<div class="step" id="detail">
+    <p><?php echo $venue->description; ?></p>
+</div>
+
+<div class="step" id="locations">
+
+<?php
+    $args = array(
+        'post_type' => 'location',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'venue',
+                'field'    => 'term_id',
+                'terms'    => array($venue->term_id),
+            ),
+        ),
+    );
+    $locations = new WP_Query( $args );
+    foreach ($locations as $location) {
+        echo '<h1>'.$location->post_title.'</h1>';
+        echo '<p>'.$location->post_content.'</p>';
+        
+    }
+?>
+
+</div>
+
+<div class="step" id="gifts">
+    <h1>Gifts made for this venue</h1>
 </div>
 
 <div style="position: fixed; right: 20px; bottom: 20px;">
@@ -16,8 +43,12 @@
 jQuery(function($) {
 	$.backstretch('<?php echo get_stylesheet_directory_uri(); ?>/images/backstretch/index-project.jpg');
 
-    $('#venue').fadeIn(function () {
+    $('#detail').fadeIn(function () {
+        $('#locations').fadeIn(function () {
+            $('#gifts').fadeIn(function () {
         
+            });
+        });
     });
 });
 </script>
