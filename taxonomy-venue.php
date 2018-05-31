55d3c90b1a1b5887c2e3ff3a9edd333e4315f13d
growlingfish <github@growlingfish.com>
2018-05-31 13:45:04 +0100

    Venue

--- taxonomy-venue.php
+++ taxonomy-venue.php
@@ -31,6 +31,7 @@ add_filter( 'genesis_site_layout', '__genesis_return_full_width_content' );
 remove_action( 'genesis_header', 'genesis_header_markup_open', 5 );
 remove_action( 'genesis_header', 'genesis_do_header' );
 remove_action( 'genesis_header', 'genesis_header_markup_close', 15 );
+remove_action( 'genesis_before_loop', 'genesis_do_taxonomy_title_description', 15 );
 
 // Remove navigation.
 remove_theme_support( 'genesis-menus' );
--- templates/single-venue.php
+++ templates/single-venue.php
@@ -5,7 +5,9 @@
 <script src="https://unpkg.com/isotope-layout@3.0.6/dist/isotope.pkgd.min.js"></script>
 
 <div class="step" id="detail">
-    <p><?php echo $venue->description; ?></p>
+    <h1><?php echo $venue->name; ?></h1>
+    <p>Introduced with:</p>
+    <blockquote><?php echo $venue->description; ?></blockquote>
 </div>
 
 <div class="step" id="locations">
@@ -22,17 +24,122 @@
         ),
     );
     $locations = new WP_Query( $args );
+    $query = array(
+        'numberposts'   => -1,
+        'post_type'     => 'gift',
+        'post_status'   => 'publish'
+    );
+    $all_gifts = get_posts( $query );
     foreach ($locations as $location) {
-        echo '<h1>'.$location->post_title.'</h1>';
+        echo '<h2>'.$location->post_title.'</h2>';
         echo '<p>'.$location->post_content.'</p>';
-        
+        echo '<div class="grid">';
+        foreach ($all_gifts as $gift) {
+            $wraps = get_field( 'field_58e4f5da816ac', $gift->ID);
+            if ($wraps) {
+                foreach ($wraps as $wrap) {
+                    unset ($object);
+                    $object = get_field( 'field_595b4a2bc9c1c', $wrap->ID);
+                    if (is_array($object) && count($object) > 0) {
+                        $object = $object[0];
+                    } else if (is_a($object, 'WP_Post')) {
+                            
+                    } else {
+                        unset($object);
+                    }
+
+                    if ($object) {
+                        $l = get_field( 'field_59a85fff4be5a', $post->ID );
+                        if (!$l || count($l) == 0) {
+                            return null;
+                        }
+                        $l = $l[0];
+                        if ($l->ID == $location->ID) {
+                            echo $object->post_title;
+                        }
+                    }
+                }
+            }
+        }
+        echo '</div>';
     }
 ?>
 
 </div>
 
 <div class="step" id="gifts">
-    <h1>Gifts made for this venue</h1>
+    <h2>Gifts made for this venue</h2>
+    <div class="grid">
+<?php
+    $args = array(
+        'post_type' => 'location',
+        'tax_query' => array(
+            array(
+                'taxonomy' => 'venue',
+                'field'    => 'term_id',
+                'terms'    => array($venue->term_id),
+            ),
+        ),
+    );
+    $locations = new WP_Query( $args );
+    $query = array(
+        'numberposts'   => -1,
+        'post_type'     => 'gift',
+        'post_status'   => 'publish'
+    );
+    $all_gifts = get_posts( $query );
+    foreach ($all_gifts as $gift) {
+        $wraps = get_field( 'field_58e4f5da816ac', $gift->ID);
+        if ($wraps) {
+            foreach ($wraps as $wrap) {
+                unset ($object);
+                $object = get_field( 'field_595b4a2bc9c1c', $wrap->ID);
+                if (is_array($object) && count($object) > 0) {
+                    $object = $object[0];
+                } else if (is_a($object, 'WP_Post')) {
+                        
+                } else {
+                    unset($object);
+                }
+
+                if ($object) {
+                    $l = get_field( 'field_59a85fff4be5a', $post->ID );
+                    if (!$l || count($l) == 0) {
+                        return null;
+                    }
+                    $l = $l[0];
+                    if ($l->ID == $location->ID) {
+                        $senderdata = get_userdata($gift->post_author);
+
+                        // recipient
+                        $recipients = get_field( 'field_58e4f6e88f3d7', $gift->ID );
+                        unset($recipientdata);
+                        if ($recipients) {
+                            foreach ($recipients as $recipient) {
+                                $recipientdata = get_userdata($recipient['ID']);
+                                break; // only one recipient for now
+                            }
+                        }
+
+                        echo '<div class="grid-item '
+                            .(isset ($gift->post_modified) && isset ($senderdata->nickname) && isset ($recipientdata->nickname) ? 'complete' : 'incomplete')
+                            .'" gift="'.$gift->ID.'"'                    
+                        .'>'
+                            .'<strong>Gift #'.$gift->ID.'</strong>'
+                            .'<ul>'
+                                .'<li>Sent: '.(isset ($gift->post_modified) ? $gift->post_modified : '<span style="color: red">No date</span>' ).'</li>'
+                                .'<li>By: '.(isset ($senderdata->nickname) ? urldecode($senderdata->nickname) : '<span style="color: red">No sender</span>' ).'</li>'
+                                .'<li>To: '.(isset ($recipientdata->nickname) ? urldecode($recipientdata->nickname) : '<span style="color: red">No recipient</span>' ).'</li>'
+                            .'</ul>'
+                        .'</div>';
+                        break;
+                    }
+                }
+            }
+        }
+    }
+?>
+    </div>
 </div>
 
 <div style="position: fixed; right: 20px; bottom: 20px;">
