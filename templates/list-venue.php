532a67f9bb5c2a81a0b76ac313783b17300cf81f
growlingfish <github@growlingfish.com>
2018-06-01 12:35:10 +0100

    Venues

--- templates/list-venue.php
+++ templates/list-venue.php
@@ -11,11 +11,16 @@
     if (count($venues) == 0) {
         echo '<li>None</li>';
     } else {
+        $i = 0;
         foreach ($venues as $venue) {
-            $owner = get_field( 'owner', $venue->term_id );
-            if ($owner->data->ID == get_current_user_id()) {
+            $owner = get_field( 'owner', $venue );
+            if ($owner->ID == get_current_user_id()) {
                 echo '<li>'.$venue->name.' <a href="'.get_term_link($venue->term_id, 'venue').'" class="button">View</a> <a href="'.get_edit_term_link( $venue->term_id, 'venue', 'location' ).'" class="button">Edit</a></li>';
+                $i++;
             }
+        }
+        if ($i == 0) {
+            echo '<li>None</li>';
         }
     }
 
