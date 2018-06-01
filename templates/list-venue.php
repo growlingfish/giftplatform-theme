51abff4d6a1d48a6fb411f4499699ffd742e8cd7
growlingfish <github@growlingfish.com>
2018-06-01 12:25:37 +0100

    Venues

--- templates/list-venue.php
+++ templates/list-venue.php
@@ -1,7 +1,26 @@
 <div class="step" id="venues">
     <h1>Your venues</h1>
     <p>You have registered the following venues:</p>
+    <ul>
+<?php
 
+    $venues = get_terms( array(
+        'taxonomy' => 'venue',
+        'hide_empty' => false,
+    ) );
+    if (count($venues) == 0) {
+        echo '<li>None</li>';
+    } else {
+        foreach ($venues as $venue) {
+            $owner = get_field( 'owner', $venue->term_id );
+            if ($owner->data->ID == get_current_user_id()) {
+                echo '<li>'.$venue->name.' <a href="'.get_term_link($venue->term_id, 'venue').'" class="button">View</a> <a href="'.get_edit_term_link( $venue->term_id, 'venue', 'location' ).'" class="button">Edit</a></li>';
+            }
+        }
+    }
+
+?>
+    </ul>
 </div>
 
 <div style="position: fixed; right: 20px; bottom: 20px;">
