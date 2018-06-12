597434d3fe27383f61ecd48e2e8ac8e681a48442
growlingfish <github@growlingfish.com>
2018-06-12 11:26:33 +0100

    Vis

--- templates/report-exchvis.php
+++ templates/report-exchvis.php
@@ -231,7 +231,7 @@ function dragended(d) {
 }
 
 function getNeighbors(node) {
-  return linkElements.reduce((neighbors, link) => {
+  return links.reduce((neighbors, link) => {
     if (link.target.id === node.id) {
       neighbors.push(link.source.id)
     } else if (link.source.id === node.id) {
