885bf7a4f15d105ecc4f46ff22b5e6c2be60993e
Ben Bedwell <github@growlingfish.com>
2018-07-05 14:05:45 +0100

    CMS

--- templates/cms-location.php
+++ templates/cms-location.php
@@ -15,9 +15,9 @@
     <p>"Locations" are a way of dividing up your venue into different spaces: this then allows you to assign objects to particular spaces, making it easier for visitors to organise the content of their gifts.</p>
     <p>Click on the button below to open the Gift CMS. In the new page ...</p>
     <ol>
-        <li style="list-style-type: numeric;">Give the location a name (in the box labelled "Enter title here")</li>
-        <li style="list-style-type: numeric;">Give the location a brief description in the larger box below</li>
-        <li style="list-style-type: numeric;">Start typing the name of your venue <?php echo (isset ($venue) ? '('.$venue->name.')' : '' ); ?> into the box labelled "Venues"; when your venue appears, "add" it</li>
+        <li style="list-style-type: numeric !important;">Give the location a name (in the box labelled "Enter title here")</li>
+        <li style="list-style-type: numeric !important;">Give the location a brief description in the larger box below</li>
+        <li style="list-style-type: numeric !important;">Start typing the name of your venue <?php echo (isset ($venue) ? '('.$venue->name.')' : '' ); ?> into the box labelled "Venues"; when your venue appears, "add" it</li>
     </ol>
     <p style="padding-top: 30px;">When this is complete, press the button labelled "Publish".</p>
     <p><a href="https://gifting.digital/wp/wp-admin/post-new.php?post_type=location" class="button">Open the CMS</a></p>
